1447dd7c71cef0f49bb44273d5f9c596a2c0b0f4
Rokeybur Rahman <28459919+rokeyfx@users.noreply.github.com>
2020-08-31 09:35:32 +0600

    Update floatingCallNow.js

--- floatingCallNow.js
+++ floatingCallNow.js
@@ -3,7 +3,7 @@ setTimeout(function(){
 	var link = document.createElement('link'); 
 	link.rel = 'stylesheet';  
 	link.type = 'text/css'; 
-	link.href = 'floatingCallNow.css';  
+	link.href = 'https://raw.githubusercontent.com/rokeyfx/floatingcallbutton/master/floatingCallNow.css';  
 	document.getElementsByTagName('body')[0].appendChild(link);
 
     document.body.innerHTML += '<div class="call-now-button-st" style="bottom:'+ positionBottom + ';left:'+ positionLeft + ';right:'+ positionRight + ';top:'+ positionTop + ';background:'+ callFloatingBackground + ';z-index:'+ callFloatingZindex + ';"><div><a class="phone-no-st" href="tel:' + phoneNumber + '" id="quickcallbutton" title="Call Now"><p class="call-text"> ' + callText + ' </p><div class="quick-alo-ph-circle active"></div>            <div class="quick-alo-ph-circle-fill active"></div>            <div class="quick-alo-ph-img-circle shake" style="background:' + callIconBackground + '"></div>        </a>    </div></div>'
