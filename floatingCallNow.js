6b73ba40f635a48e3147f509d06294b45d3923b1
Rokeybur Rahman <28459919+rokeyfx@users.noreply.github.com>
2020-08-31 09:16:55 +0600

    Add files via upload

---
+++ floatingCallNow.js
@@ -0,0 +1,15 @@
+setTimeout(function(){ 
+
+	var link = document.createElement('link'); 
+	link.rel = 'stylesheet';  
+	link.type = 'text/css'; 
+	link.href = 'floatingCallNow.css';  
+	document.getElementsByTagName('body')[0].appendChild(link);
+
+    document.body.innerHTML += '<div class="call-now-button-st" style="bottom:'+ positionBottom + ';left:'+ positionLeft + ';right:'+ positionRight + ';top:'+ positionTop + ';background:'+ callFloatingBackground + ';z-index:'+ callFloatingZindex + ';"><div><a class="phone-no-st" href="tel:' + phoneNumber + '" id="quickcallbutton" title="Call Now"><p class="call-text"> ' + callText + ' </p><div class="quick-alo-ph-circle active"></div>            <div class="quick-alo-ph-circle-fill active"></div>            <div class="quick-alo-ph-img-circle shake" style="background:' + callIconBackground + '"></div>        </a>    </div></div>'
+
+
+}, 1000);
+
+
+
